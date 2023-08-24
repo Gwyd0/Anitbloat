ed3002ce1e6dd31f4e8b756bd83c20dffeb07106
Gwydion <68437867+Gwyd0@users.noreply.github.com>
2023-08-24 17:04:07 +0100

    Can now pick actions

--- start.sh
+++ start.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-BLACK="\033[30m"
 RED="\033[31m"
 GREEN="\033[32m"
 YELLOW="\033[33m"
@@ -11,62 +10,73 @@ NORMAL="\033[0;39m"
 NAME="$CYAN[ANTIBLOAT]$NORMAL"
 ERROR="$RED[ERROR]$NORMAL"
 SUCCESS="$GREEN[SUCCESS]$NORMAL"
-V="Version. 1.0.1 -$YELLOW For Huawei only.$NORMAL"
-DEVICE=""
+V="Version. 1.0.2 -$YELLOW Shell$NORMAL"
+
+EDR="1"
 
 removebloatware() {
-  echo -e $NAME IF anything breaks or stop working you can re-enable the packages.\ Connecting to device...
-  currentApps=($(adb -d shell cmd package list packages))
-  name=$1[@]
-  a=("${!name}")
-  for bloatware in "${a[@]}"
+  bloatware=$(jq -r '.bloatware.'$1'[] | .package' bloatware.json)
+  apps=$(adb shell pm list packages)
+  
+  if [ $EDR == "0" ]; then
+    STRING="Enabl"
+    COMMAND=$(eval "adb shell pm enable --user 0 $app | grep -q 'Success'")
+  elif [ $EDR == "1" ]; then
+    STRING="Disabl"
+    COMMAND=$(eval "adb shell pm disable-user --user 0 $app | grep -q 'Success'")
+  else
+    STRING="Remov"
+    COMMAND=$(eval "adb shell pm uninstall --user 0 $app | grep -q 'Success'")
+  fi
+  
+  printf "\n$CYAN-------------$NORMAL\n"
+  
+  for bloat in $bloatware:
   do
-    for app in ${currentApps[@]//package:}
+    for app in ${apps[@]//package:}
     do
-      if [ "$app" == "$bloatware" ]; then
-        echo -e $NAME Would you like to Disable $YELLOW$app$RED Y/n$NORMAL 
-        read -p '>: ' input
-        case $input in
-        [Yy]* ) 
-            echo -e $NAME Disabling package $YELLOW$app$NORMAL
-            if adb -d shell pm disable-user --user 0 $app | grep -q 'Success'; 
-            then
-              echo -e $SUCCESS Disabled package $YELLOW$app$NORMAL
-            else 
-              echo -e $ERROR Could not Disable package $YELLOW$app$NORMAL
-            fi 
-            printf "\n$CYAN-------------$NORMAL\n"
-        ;;
-        [Nn]* ) 
-            echo -e $NAME You answered no, Skipping
-            printf "\n$CYAN-------------$NORMAL\n"
-            break
-        ;;
-        * ) echo -e $ERROR Invalid, use y/n Skipping;;
-    esac
+      if [ "$app" == "${bloat//:}" ]; then
+        echo -e $NAME Would you like to $STRING\e $YELLOW$app$RED Y/n$NORMAL 
+	  read -p '>: ' input
+	  case $input in
+	  [Yy]* ) 
+	      echo -e $NAME $STRING\ing package $YELLOW$app$NORMAL
+	      if $COMMAND; 
+	      then
+	        echo -e $SUCCESS $STRING\ed package $YELLOW$app$NORMAL
+	      else 
+	        echo -e $ERROR Could not $STRING\e package $YELLOW$app$NORMAL
+	      fi 
+	      printf "\n$CYAN-------------$NORMAL\n"
+	  ;;
+	  [Nn]* ) 
+	      echo -e $NAME You answered No, Skipping
+	      printf "\n$CYAN-------------$NORMAL\n"
+	  ;;
+	  * ) echo -e $ERROR Invalid, use y/n Skipping;;
+         esac
       fi
     done
   done
 }
 
-
-
 echo -e $NAME $V
-echo -e $NAME By$CYAN Gwyd$GREEN Github:$PINK https://github.com/Gwyd0/Anitbloat$NORMAL
-echo -e  $NAME Connecting to device using ADB$YELLOW Make sure you have ADB installed and a device connected via USB with USB debugging enabled.$NORMAL
-
-while adb -d get-state devices | grep -q 'device' 
-do
-
-  echo -e $NAME Locating$YELLOW Huawei$NORMAL bloatware 
-  printf "\n$CYAN-------------$NORMAL\n"
-  mapfile -t huewaiBloatware < huewaiB.txt
-  removebloatware huewaiBloatware
-  
-  echo -e $NAME Locating$YELLOW Google$NORMAL bloatware 
-  printf "\n$CYAN-------------$NORMAL\n"
-  mapfile -t googleBloatware < googleB.txt
-  removebloatware googleBloatware
-  exit
-
-done
+echo -e $NAME By$CYAN Gwyd$GREEN Github:$CYAN https://github.com/Gwyd0/Anitbloat$NORMAL
+echo -e  $NAME Connecting to device using ADB$GREEN Make sure you have ADB installed and a device connected via USB with USB debugging enabled.$NORMAL
+printf "\n$CYAN-------------$NORMAL\n"
+echo -e $NAME Are you Enabling/Disabling/Removing apps?$RED 0/1/2$NORMAL 
+	  read -p '>: ' input
+	  if [ $input == "0" ] || [ $input == "1" ] || [ $input == "2" ]; then
+	    echo -e $NAME You picked$GREEN $input
+	    EDR="$input"
+	  else
+	    echo -e $ERROR Please pick 0/1/2 Going with default,$CYAN Disabling $NORMAL
+	  
+	  fi
+printf "\n$CYAN-------------$NORMAL\n"
+removebloatware "huawei"
+removebloatware "google"
+removebloatware "amazon"
+echo -e $NAME Done! Removed/Disabled/Enabled all bloatware.
+echo -e $NAME Exiting
+exit
