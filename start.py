c90a23b1deb1bd54ffd4bc33eca563745f3502c4
Gwydion <68437867+Gwyd0@users.noreply.github.com>
2023-08-25 15:12:16 +0100

    V 1.0.3

--- start.py
+++ start.py
@@ -23,6 +23,8 @@ def removebloatware(com):
     with open("bloatware.json", "r") as read:
         data = json.load(read)
 
+
+
         if d.EDR == 0:
             string = "enable"
         elif d.EDR == 1:
@@ -30,34 +32,39 @@ def removebloatware(com):
         else:
             string = "remove"
 
-        print(d.EDR)
-        for a in data["bloatware"][com]:
-            app = a["package"]
-            desc = a["desc"]
-            a = input(
-                d.NAME + "Would you like to " + string + " {0}{1}? {2}({3})\n>:".format(d.YELLOW, app, d.RED, desc))
+        adb_bloatware = subprocess.check_output(["adb", "-d", "shell", "pm", "list", "packages"]).decode("utf-8")
+        bloatware = adb_bloatware.split("package:")
+        for bloat in bloatware:
+            for a in data["bloatware"][com]:
+                app = a["package"]
+                desc = a["desc"]
+                if app == bloat.strip():
+                    a = input(
+                        d.NAME + "Would you like to " + string + " {0}{1}? {2}({3})\n>:".format(d.YELLOW, app, d.RED, desc))
 
-            if a.casefold() == "y":
-                print(d.NAME + string[0:-1] + "ing package {0}{1}".format(d.YELLOW, app))
-                if d.EDR == 0:
-                    adb = subprocess.run(["adb", "-d", "shell", "pm", "enable-user", "--user", "0", app],
-                                         stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
-                elif d.EDR == 1:
-                    adb = subprocess.run(["adb", "-d", "shell", "pm", "disable-user", "--user", "0", app],
-                                         stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
-                else:
-                    adb = subprocess.run(["adb", "-d", "shell", "pm", "uninstall", "--user", "0", app],
-                                         stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
-                if not adb.returncode == 0:
-                    print(d.ERROR + "Could not" + string + "package {0}{1}".format(d.YELLOW, app))
-                else:
-                    print(d.SUCCESS + string + "d package {0}".format(app))
-            elif a.casefold() == "n":
-                print(d.NAME + "Skipping Package")
-            else:
-                print(d.ERROR + "Invalid Argument Skipping..")
+                    if a.casefold() == "y":
+                        print(d.NAME + string[0:-1] + "ing package {0}{1}".format(d.YELLOW, app))
+                        if d.EDR == 0:
+                            adb = subprocess.run(["adb", "-d", "shell", "pm", "enable-user", "--user", "0", app],
+                                                 stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
+                        elif d.EDR == 1:
+                            adb = subprocess.run(["adb", "-d", "shell", "pm", "disable-user", "--user", "0", app],
+                                                 stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
+                        else:
+                            adb = subprocess.run(["adb", "-d", "shell", "pm", "uninstall", "--user", "0", app],
+                                                 stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
+                        if not adb.returncode == 0:
+                            print(d.ERROR + "Could not" + string + "package {0}{1}".format(d.YELLOW, app))
+                        else:
+                            print(d.SUCCESS + string + "d package {0}".format(app))
+                    elif a.casefold() == "n":
+                        print(d.NAME + "Skipping Package")
+                    else:
+                        print(d.ERROR + "Invalid Argument Skipping..")
 
-            print("\n{0}----------------{1}\n".format(d.CYAN, d.NORMAL))
+                    print("\n{0}----------------{1}\n".format(d.CYAN, d.NORMAL))
+                else:
+                    break
 
 
 print(d.NAME + d.v)
@@ -70,7 +77,6 @@ print("{0}By{1} Gwyd{2} Github:{3} https://github.com/Gwyd0/Anitbloat{4}\n{0}Con
 print("\n{0}----------------{1}\n".format(d.CYAN,d.NORMAL))
 
 b = input(d.NAME+"Are you {0}enabling/disabling/removing apps?{1} (0,1,2)\n>:".format(d.CYAN,d.RED))
-
 if b=="0" or b=="1" or b=="2":
     d.EDR = int(b)
     print(d.NAME+"You picked "+b)
